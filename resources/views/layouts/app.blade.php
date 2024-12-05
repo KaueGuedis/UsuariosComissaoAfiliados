41997cef3a6b5eaae77494f7fb4a187a33a006c2
KaueGuedis <kaueguedes2904@gmail.com>
2024-12-05 13:00:15 -0300

    Ajuste Logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RealsBet</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="icon" href="{{ asset("storage/images/icon_realsBet.png") }}">
+    <link rel="icon" href="{{ asset("images/icon_realsBet.png") }}">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
+            <img src="{{ asset("images/logo_realsBet.png") }}" style="width: 2%`; margin-right: 1%">
             <a class="navbar-brand" href="#">RealsBet</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
